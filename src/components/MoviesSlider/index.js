3d3bf724bfe0c00ed810a708d50b2d46fca9ebab
JaiKumar1003 <156499686+JaiKumar1003@users.noreply.github.com>
2024-03-19 19:51:21 +0530

    Output Error

--- src/components/MoviesSlider/index.js
+++ src/components/MoviesSlider/index.js
@@ -4,11 +4,16 @@ import Slider from 'react-slick'
 
 import MovieItem from '../MovieItem'
 
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+import './index.css'
+
 const MoviesSlider = props => {
   const {moviesList} = props
 
   const settings = {
-    slidesToShow: 1,
+    slidesToShow: 4,
     slidesToScroll: 1,
   }
   return (
